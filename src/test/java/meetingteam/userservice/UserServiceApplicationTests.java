2b878eab0bd47a6ba1f9727ef4244442df9cb105
HungTran170904 <tienhung17092004@gmail.com>
2025-03-20 08:39:40 +0700

    feat: add unit test

--- src/test/java/meetingteam/userservice/UserServiceApplicationTests.java
+++ src/test/java/meetingteam/userservice/UserServiceApplicationTests.java
@@ -1,13 +1,35 @@
 package meetingteam.userservice;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import meetingteam.userservice.dtos.User.CreateUserDto;
+
+@SpringBootConfiguration
 class UserServiceApplicationTests {
+    private final Validator validator;
+
+    public UserServiceApplicationTests() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        this.validator = factory.getValidator();
+    }
 
     @Test
     void contextLoads() {
-    }
+        var createUserDto= new CreateUserDto();
+        createUserDto.setNickName("HungTran");
+        createUserDto.setEmail("tienhung170904@gmail.com");
+        createUserDto.setGender(false);
 
+        Set<ConstraintViolation<CreateUserDto>> violations = validator.validate(createUserDto);
+        assertTrue(!violations.isEmpty(), "DTO should be invalid");
+    }
 }
