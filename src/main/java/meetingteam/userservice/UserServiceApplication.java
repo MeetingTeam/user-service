aed6903bda307729372ad7e18d059f90cf55aeeb
HungTran170904 <tienhung17092004@gmail.com>
2025-04-25 16:57:11 +0700

    docs: comment some things

--- src/main/java/meetingteam/userservice/UserServiceApplication.java
+++ src/main/java/meetingteam/userservice/UserServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserServiceApplication {
 
+    // Comment some thing
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
